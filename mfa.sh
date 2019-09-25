94b7cd4389b97b823f4778ceeccd30aa86f7b0be
Emir Ercan Ayar <eercanayar@gmail.com>
2019-09-25 13:16:38 +0300

    added duration parameter

--- mfa.sh
+++ mfa.sh
@@ -36,7 +36,10 @@ if [ ! -r ~/mfa.cfg ]; then
   exit 2
 fi
 
-AWS_CLI_PROFILE=${2:-default}
+AWS_CLI_PROFILE=${3:-default}
+ACCESS_DURATION=${2:480}
+ACCESS_DURATION=ACCESS_DURATION*60
+
 MFA_TOKEN_CODE=$1
 ARN_OF_MFA=$(grep "^$AWS_CLI_PROFILE" ~/mfa.cfg | cut -d '=' -f2- | tr -d '"')
 
@@ -45,6 +48,6 @@ echo "MFA ARN: $ARN_OF_MFA"
 echo "MFA Token Code: $MFA_TOKEN_CODE"
 
 echo "Your Temporary Creds:"
-aws --profile $AWS_CLI_PROFILE sts get-session-token --duration 129600 \
+aws --profile $AWS_CLI_PROFILE sts get-session-token --duration $ACCESS_DURATION \
   --serial-number $ARN_OF_MFA --token-code $MFA_TOKEN_CODE --output text \
   | awk '{printf("export AWS_ACCESS_KEY_ID=\"%s\"\nexport AWS_SECRET_ACCESS_KEY=\"%s\"\nexport AWS_SESSION_TOKEN=\"%s\"\nexport AWS_SECURITY_TOKEN=\"%s\"\n",$2,$4,$5,$5)}' | tee ~/.token_file
